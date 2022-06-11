9c880163dfffaec10e971fde9a5e496ebdf11711
+ <+>
2022-06-12 00:15:29 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats= ['Milo' ,'Otis', 'Garfield']
+
+function destructivelyAppendCat (name) {
+    cats.push (name);
+}
+
+function destructivelyPrependCat (name){
+    cats.unshift (name);
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop ();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift ();
+}
+
+function appendCat (name){
+    return cats.concat (name);
+}
+
+function prependCat (name){
+    const newCats = [name ,...cats]
+}
+
+function removeLastCat(){
+    return cats.slice (0,cats,length)
+}
